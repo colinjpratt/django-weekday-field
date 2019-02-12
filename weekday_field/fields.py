cc37f58a35559c58d125027446d99b30912452f7
colinjpratt <colinp@mediarain.com>
2019-02-11 23:14:27 -0700

    forgot to import the validator

--- weekday_field/fields.py
+++ weekday_field/fields.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import validate_comma_separated_integer_list
 from weekday_field.forms import WeekdayFormField
 from weekday_field import utils
 
